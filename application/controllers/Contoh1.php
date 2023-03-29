d8d720180116ee2ffc93eb3c7cb563afc37432aa
vikry234 <vikry234@gmail.com>
2023-03-29 13:18:45 +0700

    update

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo    "<h1>Perkenalkan</h1>";
+        echo    "Nama saya vikry maulana
+                saya tinggal di daerah bogor
+                hobi saya menggambar";
+    }
+}
